07979564da1d5a1b4410b80391d39a657ec99a39
Pedro Spim <pepespim06@gmail.com>
2021-01-19 12:53:44 -0300

    Cobertura de ate 55% do app

--- StarMovies/StarMoviesTests/StarMoviesTests.swift
+++ StarMovies/StarMoviesTests/StarMoviesTests.swift
@@ -23,17 +23,38 @@ class StarMoviesTests: XCTestCase {
         
         let expectation = self.expectation(description: "Uma descricao")
         
-        FilmeAPI().pegarListaTendenciasFilmes(pagina: 1) { (resposta) in
-            XCTAssertEqual(resposta.results?.count, 20)
-            expectation.fulfill()
+        for indice in 1...4 {
+            FilmeAPI().pegarListaTendenciasFilmes(pagina: indice) { (resposta) in
+                let viewData: Bindable<TendenciasViewData?> = Bindable(nil)
+                viewData.value = TendenciasViewData(model: resposta)
+                
+                XCTAssertEqual(viewData.value?.resultados.count, 20)
+                if indice == 3 {
+                    expectation.fulfill()
+                }
+            }
         }
-        
-        self.waitForExpectations(timeout: 5)
+        self.waitForExpectations(timeout: 15)
     }
 
     func testExample() throws {
 
     }
+    
+    func testDetalhesFilmes(){
+        let expectation = self.expectation(description: "Uma descricao")
+        FilmeAPI().pegarDetalhesFilme(codFilme: 464052) { (resposta) in
+            let viewData: Bindable<FilmeViewData?> = Bindable(nil)
+            viewData.value = FilmeViewData(model: resposta)
+            
+            XCTAssertEqual(464052, resposta.id)
+            XCTAssertEqual("Mulher-Maravilha 1984", viewData.value?.title)
+            XCTAssertEqual("$200,000,000.00", viewData.value?.budget)
+            expectation.fulfill()
+        }
+        self.waitForExpectations(timeout: 5)
+    }
+    
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
